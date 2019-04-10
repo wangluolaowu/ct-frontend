2fad00df57a934dd5edddd5fd4319e1872d0c522
lv1234 <604498933@qq.com>
2019-04-10 12:55:29 +0800

    增加login后缀

--- src/util/baseData.js
+++ src/util/baseData.js
@@ -1,12 +1,13 @@
 'use strict'
+
 module.exports = {
-  //  baseURL: 'http://localhost:8080',
+  // baseURL: 'http://localhost:8080',
   baseURL: 'https://bixi-test.cn.isn.corpintra.net/ct',
   // baseURL: 'http://172.16.18.97:8080/',
   // wsURL: 'localhost:8080',
   wsURL: 'bixi-test.cn.isn.corpintra.net/ct',
   // loginUrl: 'http://localhost:8081/#/login',
-  loginUrl: 'https://bixi-test.cn.isn.corpintra.net/ct',
+  loginUrl: 'https://bixi-test.cn.isn.corpintra.net/ct/login',
   token: '',
   userSite: []
 }
